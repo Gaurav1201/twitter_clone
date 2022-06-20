a4676d912dc84a6f17b7780cff9f84c295c89498
Gaurav Acharya <68782320+Gaurav1201@users.noreply.github.com>
2022-06-20 19:34:34 +0530

    Create app.js
    Button1 is the component
    not defined

---
+++ frontend/app.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import React, {Component} from 'react';
+import './Button1.js' ;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <table>
+        <tr>
+          <td>root.render(<Button1 name= 'Hello'/>)</td>
+          <td>_</td>
+          <td><Button1 name='Hello' /></td>
+          <td>_</td>
+          <td><Button1 /></td>
+        </tr>
+       
+        <tr>
+          <td><Button1 />|</td>
+          <td>_</td>
+          <td><Button1 />|</td>
+          <td>_</td>
+          <td><Button1 />|</td>
+        </tr>
+        
+        <tr>
+        <td><Button1 />|</td>
+          <td>_</td>
+          <td><Button1 />|</td>
+          <td>_</td>
+          <td><Button1 />|</td>
+        </tr>
+       </table>
+      </header>
+    </div>
+  );
+  }
+
+
+
+export default App;
